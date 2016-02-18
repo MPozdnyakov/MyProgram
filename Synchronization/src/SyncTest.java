6bbceac90a1b8bebd647d70a2d3a6523a536bc4c
Mike <Mpozdnjakov25@gmail.com>
2016-02-18 10:00:47 +0200

    new

--- Synchronization/src/SyncTest.java
+++ Synchronization/src/SyncTest.java
@@ -5,50 +5,57 @@ import com.sun.org.apache.xpath.internal.SourceTree;
  */
 public class SyncTest {
 
-    public static void main(String[] args) {
-        CustomMutex mutex = new CustomMutex();
+    public static void main(String[] args) throws InterruptedException {
 
-        Thread thread = new Thread(new Counter(mutex));
-        Thread thread2 = new Thread(new Counter(mutex));
-        Thread thread3 = new Thread(new Counter(mutex));
+
+        Thread thread = new Thread(new Counter(0));
+        Thread thread2 = new Thread(new Counter(1));
+        Thread thread3 = new Thread(new Counter(2));
 
         thread.start();
         thread2.start();
         thread3.start();
 
+        thread.join();
+        thread2.join();
+        thread3.join();
     }
 
 }
 
 class Counter implements Runnable {
-    int counter = 0;
-    CustomMutex mutex;
 
-    public Counter(CustomMutex mutex) {
-        this.mutex =mutex;
+
+     private int adding;
+     private int switcher = 0;
+
+    public Counter(int adding) {
+        this.adding = adding;
     }
+
 
     @Override
     public void run() {
-        synchronized (mutex) {
-                            for (int i = 0; i < 5; i++) {
+        for (int i = 1 + adding; i < 20; i += 3) {
+            if (adding == 0 && switcher == 0) {
 
-                    System.out.println(Thread.currentThread().getName() + " " + numbersCounter());
+                System.out.println(Thread.currentThread().getName() + " " + i);
+
+            } switcher = 1;
+
+            if (adding == 1 && switcher == 1) {
+
+                System.out.println(Thread.currentThread().getName()+" "+i);
 
             }
+            switcher = 2;
 
+            if (adding == 2 && switcher == 2) {
 
-    }
-    }
+                System.out.println(Thread.currentThread().getName()+" "+i);
 
-    private int numbersCounter() {
-        {
-            counter++;
+            }
+            switcher = 0;
         }
-        return counter;
     }
-}
-
-class CustomMutex{
-    int i =0;
 }
